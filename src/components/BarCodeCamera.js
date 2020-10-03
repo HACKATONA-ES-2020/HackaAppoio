22b9d4dbf17c78006cc7f0ba4c899a1cfdbac733
ecardoso <eduardo.ferreira98@pucrs.br>
2020-10-03 18:20:02 -0300

    QR Code reader component

---
+++ src/components/BarCodeCamera.js
@@ -0,0 +1,84 @@
+/**
+ * Componente que realiza a leitura de QRs Codes
+ *
+ * @author            Eduardo Cardoso
+ * @example           <BarcodeCamera onChange={(data) => console.log(data)} />
+ */
+
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+const DEVICE_WIDTH = Dimensions.get("window").width;
+const DEVICE_HEIGHT = Dimensions.get("window").height;
+
+export default function BarcodeCamera({ onChange }) {
+  const [CameraPermissionGranted, setCameraPermissionGranted] = useState(null);
+  const [codeScanned, setCodeScanned] = useState(false);
+
+  async function getPermission() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    status === "granted"
+      ? setCameraPermissionGranted(true)
+      : setCameraPermissionGranted(false);
+  }
+  useEffect(() => {
+    getPermission();
+  }, []);
+
+  const QRCodeScanned = ({ data }) => {
+    setCodeScanned(true);
+    onChange(data);
+    alert(data);
+  };
+
+  return (
+    <View>
+      {CameraPermissionGranted === null && (
+        <>
+          <View style={styles.container}>
+            <Text>Please grant Camera permission</Text>
+          </View>
+        </>
+      )}
+
+      {!CameraPermissionGranted && (
+        <>
+          <View style={styles.container}>
+            <Text>Camera Permission Denied.</Text>
+          </View>
+        </>
+      )}
+
+      {CameraPermissionGranted && !codeScanned && (
+        <>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <BarCodeScanner
+              onBarCodeScanned={QRCodeScanned}
+              style={{
+                height: DEVICE_HEIGHT / 2.5,
+                width: DEVICE_WIDTH + 2,
+              }}
+            ></BarCodeScanner>
+          </View>
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
