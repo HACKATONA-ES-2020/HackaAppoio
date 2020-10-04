043920d4266665aba7d52895b9e785397f672c9b
Joao Leao <joao.leao@edu.pucrs.br>
2020-10-03 21:37:50 -0300

    ExitScreen created

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
-import ModelScreen from "./screens/ModelScreen";
+import ExitScreen from "./screens/ExitScreen";
 import PositionScreen from "./screens/PositionScreen";
 import QRCodeReaderScreen from "./screens/QRCodeReaderScreen";
 import EnterQueueScreen from "./screens/EnterQueueScreen";
@@ -13,13 +13,13 @@ export function Screens() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="ModelScreen" component={ModelScreen} />
         <Stack.Screen name="EnterQueueScreen" component={EnterQueueScreen} />
         <Stack.Screen name="PositionScreen" component={PositionScreen} />
         <Stack.Screen
           name="QRCodeReaderScreen"
           component={QRCodeReaderScreen}
         />
+        <Stack.Screen name="ExitScreen" component={ExitScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
